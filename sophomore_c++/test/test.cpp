351168bfe1b13733a0ef88bd366cb6d11b42229a
cool-breeze-0 <792110498@qq.com@example.com>
2018-10-14 23:53:20 +0800

    input -1 and enter to exit

--- sophomore_c++/test/test.cpp
+++ sophomore_c++/test/test.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 int main()
 {
+	int exit;
 	cout<<"hello world"<<endl;
+	cout<<"please input -1 and enter to exit:";
+	cin>>exit;
+	while(exit != -1)
+	{
+		cout<<"please input -1 and enter to exit£º";
+		cin>>exit; 
+	}
 	return 0;
  } 
+ 
